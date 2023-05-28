7ce843e4c3eab8b544ea933f4b5f0fd22fbfbb15
Kulpot <kulpotwahu@gmail.com>
2023-05-29 04:09:52 +0800

    Add project files.

---
+++ Minimize_to_system_tray/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Minimize_to_system_tray
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+
+            // Determines whether the cursor is in the taskbar
+            bool cursorNotInBar = Screen.GetWorkingArea(this).Contains(Cursor.Position);
+
+            if (this.WindowState == FormWindowState.Maximized && cursorNotInBar)
+            {
+                this.ShowInTaskbar = false;
+                notifyIcon.Visible = true;
+                this.Hide();
+            }
+        }
+    }
+}
